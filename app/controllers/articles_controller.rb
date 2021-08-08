a2096e8a3401db3c2b70b168f7f640e6c3731393
Aaqib Rashid <aaqibrmir@gmail.com>
2021-08-08 12:18:02 +0530

    commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,15 @@
 class ArticlesController < ApplicationController
     
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
        # @article = Article.find(params[:id])
     end
 
     def index
-        @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 3)
     end
 
     def new
@@ -20,7 +22,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
           flash[:notice] = "Article was created!"
           redirect_to @article
@@ -52,7 +54,14 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-        params.require(:article).permit(:title, :description)
+        params.require(:article).permit(:title, :description, category_ids: [])
+    end
+
+    def require_same_user
+        if current_user != @article.user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article
+        end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
- root 'pages#home'
-  
- get 'about', to: 'pages#about'
+  root 'pages#home'
+ 
+  get 'about', to: 'pages#about'
   resources :articles #, only: [:show, :index, :new, :create, :edit, :update, :destroy]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
 end
